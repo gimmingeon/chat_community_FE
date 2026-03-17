24245ed8431fd02816035f6e8cbfb9149643e062
gimmingeon <mnbqwe123987@naver.com>
2026-03-17 16:43:22 +0900

    Feat: 게시글상세페이지 css 추가

--- src/components/DetailPost.tsx
+++ src/components/DetailPost.tsx
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import type { DetailPost } from "../type/DetailPostType"
+import "../css/DetailPost.css"
 
 interface DetailPostProps {
     data: DetailPost
@@ -8,18 +9,31 @@ interface DetailPostProps {
 export default function DetailPost({ data }: DetailPostProps) {
 
     return (
-        <div>
-            <div>
-                <span>제목: {data.title}</span>
-                <span>닉네임: {data.user.nickname}</span>
-                <span>내용: {data.content}</span>
-                <span>타입: {data.postType}</span>
-                <span>작성일: {dayjs(data.createdAt).format("YYYY-MM-DD HH:mm")}</span>
-                <span>수정일: {dayjs(data.updatedAt).format("YYYY-MM-DD HH:mm")}</span>
-                <span>tag</span>
-                {data?.postHashtag?.map(hashtag => (
-                    <span>{hashtag.hashtag}</span>
-                ))}
+        <div className="detail-post-container">
+            <div className="detail-post-card">
+                <div className="detail-post-title">
+                    {data.title}
+                </div>
+
+                <div className="detail-post-meta">
+                    <span>닉네임: {data.user.nickname}</span>
+                    <span>타입: {data.postType}</span>
+                    <span>작성일: {dayjs(data.createdAt).format("YYYY-MM-DD HH:mm")}</span>
+                    <span>수정일: {dayjs(data.updatedAt).format("YYYY-MM-DD HH:mm")}</span>
+                </div>
+
+                <div className="detail-post-content">
+                    {data.content}
+                </div>
+
+                <div>
+                    {data?.postHashtag?.map((hashtag, index) => (
+                        <span className="post-tag" key={index}>
+                            <span>  </span>#{hashtag.hashtag}
+                        </span>
+                    ))}
+                </div>
+
             </div>
         </div>
     )
