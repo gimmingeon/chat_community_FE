eae89a9e6bf8afd3b98031205ee76e54fb2a0a27
gimmingeon <mnbqwe123987@naver.com>
2026-06-05 08:48:40 +0900

    Feat: 채팅모달창 css 추가

--- src/components/chatListModal.tsx
+++ src/components/chatListModal.tsx
@@ -11,14 +11,24 @@ export default function ChatModal({ onClose, chattingList }: ChatModalProps) {
 
     let navigate = useNavigate();
     return (
-        <div className="chatlist-modal-overlay">
-            <div className="chatlist-modal">
+        <div
+            className="chatlist-modal-overlay"
+            onClick={onClose}
+        >
+            <div
+                className="chatlist-modal"
+                onClick={(e) => e.stopPropagation()}
+            >
                 <h2>채팅방 리스트</h2>
 
                 <button onClick={onClose}>닫기</button>
 
                 {chattingList.map((chatRoom, index) => (
-                    <div key={chatRoom.id} onClick={() => navigate(`/chat/${chatRoom.id}`)}>
+                    <div
+                        className="chatlist-item"
+                        key={chatRoom.id}
+                        onClick={() => navigate(`/chat/${chatRoom.id}`)}
+                    >
                         <span>{index + 1}.</span>
                         <span>{chatRoom.user.nickname}의 채팅</span>
                     </div>
